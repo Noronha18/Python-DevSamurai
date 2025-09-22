9adfd7a787235899f4d685a66b644fe07e263afa
Noronha18 <146568638+Noronha18@users.noreply.github.com>
2025-09-22 15:05:49 -0300

    Update projeto_juros_composto.py

--- projeto_juros_composto.py
+++ projeto_juros_composto.py
@@ -12,4 +12,5 @@ juros = juros / 100
 
 valor_final_composto = composto(capital, juros, meses)
 print(f'O valor recebido ao final de {meses} meses e {valor_final_composto:.2f} R$' )
+
 print(f'O valor total dos juros foi de {valor_final_composto - capital:.2f} R$')
